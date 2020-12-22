8b09ff8684c96af5c079adf4ef113bb57389b886
carlospcalvo <carloscalvonazabal@gmail.com>
2020-12-22 13:54:51 -0300

    please start working ffs

--- myapp/server.js
+++ myapp/server.js
@@ -129,7 +129,7 @@ app.post('/login', function(req, res) {
         data: datos,
         timeout: 5000
     };
-    res.send(req.rawHeaders);
+    res.send(JSON.parse(req.rawHeaders));
     axios(config)
         .then(function(response) {
             res.send(JSON.stringify(response.data));
