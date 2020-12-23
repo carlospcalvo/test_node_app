d6a672c34e54acbf280fef2b024ab36348f564cf
carlospcalvo <carloscalvonazabal@gmail.com>
2020-12-23 17:08:16 -0300

    IT'S WORKING, IT'S WORKING

--- myapp/server.js
+++ myapp/server.js
@@ -32,7 +32,7 @@ app.get('/clients', function(req, res) {
 
 app.get('/orders', function(req, res) {
 
-    ConnectServiceLayer(res, 'GET', "https://172.0.1.211:50000/b1s/v1/Orders?filter=Cancelled eq 'tNO'&$top=5");
+    ConnectServiceLayer(res, 'GET', "https://172.0.1.211:50000/b1s/v1/Orders?$filter=Cancelled eq 'tNO'&$top=5"); //$filter=contains(Cancelled,'NO')&$top=5
 
 });
 
