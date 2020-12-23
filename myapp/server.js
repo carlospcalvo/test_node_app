c578e3b0717fd96a929827bd96257a8cff2b404f
carlospcalvo <carloscalvonazabal@gmail.com>
2020-12-23 15:47:54 -0300

    please start working ffs

--- myapp/server.js
+++ myapp/server.js
@@ -133,7 +133,7 @@ app.post('/login', function(req, res) {
 
     var configGet = {
         method: 'get',
-        url: "https://172.0.1.211:50000/b1s/v1/BusinessPartners('C00818')",
+        url: "https://172.0.1.211:50000/b1s/v1/BusinessPartners",
         headers: {
             'Content-Type': 'application/json',
             'Cookie': 'B1SESSION=45e246c8-446f-11eb-8000-0ef81b3704dd; ROUTEID=.node1'
